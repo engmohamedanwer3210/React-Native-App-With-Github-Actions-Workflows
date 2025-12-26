6b3b20d83b3f1b9ebfdaed8f45d184391a5cd4c9
Mohamed ElBasyouni <mohamed.elbasyouni@vodafone.com>
2025-12-26 02:30:03 +0200

    fail unit test

--- src/screens/products/Products.spec.tsx
+++ src/screens/products/Products.spec.tsx
@@ -6,7 +6,7 @@ describe('Products', () => {
   it('renders a list of products', () => {
     const { getByTestId, getAllByText } = render(<Products />);
     const list = getByTestId('productsList');
-    expect(list).toBeTruthy();
+    expect(list).toBeFalsy();
     // Check for some product names from the mock data
     expect(getAllByText(/Product [A-E]/).length).toBeGreaterThan(0);
   });
