477785018033f71dae9dace108f6a29ccaa302e7
Mohamed ElBasyouni <mohamed.elbasyouni@vodafone.com>
2025-12-26 02:31:33 +0200

    fix: unit test

--- src/screens/products/Products.spec.tsx
+++ src/screens/products/Products.spec.tsx
@@ -6,7 +6,7 @@ describe('Products', () => {
   it('renders a list of products', () => {
     const { getByTestId, getAllByText } = render(<Products />);
     const list = getByTestId('productsList');
-    expect(list).toBeFalsy();
+    expect(list).toBeTruthy();
     // Check for some product names from the mock data
     expect(getAllByText(/Product [A-E]/).length).toBeGreaterThan(0);
   });
